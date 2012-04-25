da3d1c2c88f238026ee6242ea82791d31fad9b3d
Aman Jain <aman@sunmath.com>
2012-04-25 10:35:16 -0500

    Improvements to homepage layout

--- vendor/assets/javascripts/typekit.js
+++ vendor/assets/javascripts/typekit.js
@@ -1,2 +1,3 @@
-<script type="text/javascript" src="http://use.typekit.com/yzg5rtm.js"></script>
-<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+    %script(type="text/javascript" src="http://use.typekit.com/yzg5rtm.js")
+    %script(type="text/javascript")
+      try{Typekit.load();}catch(e){}
